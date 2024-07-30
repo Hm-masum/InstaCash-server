981b79261783e788dc0beffeb1c9d67be8872204
Hm-masum <hhmasum88@gmail.com>
2024-07-30 17:36:25 +0600

    login api commit

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const jwt=require('jsonwebtoken')
-const port = process.env.PORT || 5000;
 const app = express()
+var bcrypt = require('bcryptjs');
+const port = process.env.PORT || 5000;
+
 
 const corsOptions={
     origin:['http://localhost:5173'],
@@ -50,6 +52,60 @@ async function run() {
             next()
         })
     }
+
+    //user related api
+    app.post('/users',async(req,res)=>{
+      const userData=req.body;
+      const existingUser = await usersCollection.findOne({
+        $or: [{mobile: userData.mobile}, {email: userData.email}]
+      })
+      if(existingUser){
+        return res.send({message: 'user already exists',insertedId: null})
+      }
+
+     // Hash the PIN
+      const salt = await bcrypt.genSalt(10);
+      const hashPin = await bcrypt.hash(userData.pin.toString(), salt);
+      
+      const user ={
+        name: userData.name,
+        email: userData.email,
+        mobile: userData.mobile,
+        pin: hashPin,
+        role: userData.role,
+        balance: 0,
+        status: 'pending'
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
+    app.get('/users',async (req,res)=>{
+      const result= await usersCollection.find().toArray()
+      res.send(result)
+    })
+
+    app.post("/login",async(req,res)=>{
+      const {mobile,pin}=req.body;
+
+      const user= await usersCollection.findOne({
+        $or: [{mobile}, {email: mobile}]
+      });
+      if(!user){
+        return res.status(404).send({message: 'user not found'})
+      }
+
+      const isMatch = await bcrypt.compare(pin.toString(), user.pin);
+      if(!isMatch){
+        return res.status(401).send({message: 'unauthorized access'});
+      }
+
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+      res.send({token,user})
+    })
+
+
 
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
